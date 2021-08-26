5cb723dbd9ac49a2f0b6ae5c2976cb66d59e77f7
soko <sokojiho2@gmail.com>
2021-08-26 15:51:00 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.listen(7080, function(){
+console.log('listening on 7080')
+
+
+});
+//누군가가 /pet으로 방문을 하면..
+//pet 관련된 안내문을 띄워주자
+app.get('/pet', function(요청,응답){
+    응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+
+   
+});
+
+app.get('/beauty',function(요청,응답){
+    응답.send('뷰티제품을 쇼핑할 수 있는 페이지입니다.');
+
+});
+// /하나면 홈임
+app.get('/',function(요청,응답){
+    응답.sendfile(__dirname + '/index.html')
+
+});
