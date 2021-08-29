504253b9af87a9207e19c429281bae1e86533e77
soko <sokojiho2@gmail.com>
2021-08-29 18:23:38 +0900

    삭제및 수정

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.set('view engine','ejs');
+app.use('/public', express.static('public'));
 var db;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb+srv://anxiety:anxiety09091@cluster0.tioqv.mongodb.net/todoapp?retryWrites=true&w=majority',function(에러,client){
  
-    if(에러){return console.log(에러)}
-
+    if(에러){return console.log(에러)} 
+ 
     db = client.db('todoapp');
-    db.collection('post').insertOne({ 이름 : 'john', _id : 100},function(에러,결과){
-     console.log('저장완료');
-     
-    }); 
+    //db.collection('post').insertOne({ 이름 : 'john', _id : 100},function(에러,결과){
+    //console.log('저장완료');}); 
     app.listen(7080, function(){     
         console.log('listening on 7080')    
         
-   
+    
 })
 
   
@@ -37,20 +36,62 @@ app.get('/beauty',function(요청,응답){
 });
 // /하나면 홈임
 app.get('/',function(요청,응답){
-    응답.sendfile(__dirname + '/index.html');
-
+    //응답.sendfile(__dirname + '/views/index.ejs');
+    응답.render('index.ejs');
 });
 app.get('/write',function(요청,응답){
-    응답.sendfile(__dirname + '/write.html');
-
+    //응답.sendfile(__dirname + '/views/write.ejs');
+    응답.render('write.ejs'); 
 });
-
+ 
 //어떤사람이 /add 경로로 pst요청을하면 ??를해주세요.__dirname
 app.post('/add',function(요청,응답){
     응답.send('전송완료')
+    db.collection('counter').findOne({name : '게시물갯수'},function(에러,결과){
+        console.log(결과.totalPost); 
+         
+        var 총게시물갯수 = 결과.totalPost;  
+        db.collection('post').insertOne({ _id : 총게시물갯수 + 1,제목 : 요청.body.title, 날짜 : 요청.body.date },function(에러,결과){
+            console.log('저장완료');
+            //counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함 (수정)
+            db.collection('counter').updateOne({name : '게시물갯수'},{ $inc : {totalPost:1}},function(에러,결과){
+                if(에러) {return console.log(에러)}
+                console.log(결과)   
+            });
+       })
     
-    db.collection('post').insertOne({ 제목 : 요청.body.title, 날짜 : 요청.body.date },function(에러,결과){
-     console.log('저장완료');
-    console.log(요청.body.date);  
-    console.log(요청.body.title);   
-})});  
+    });
+
+    });   
+
+app.get('/list',function(요청,응답){
+    
+    //디비에 저장된 post라는 collection안의 모든 데이터를 꺼내주세요
+    db.collection('post').find().toArray(function(에러,결과){
+    console.log(결과); 
+    응답.render('list.ejs', {posts : 결과});  
+    });
+});  
+
+app.delete('/delete',function(요청,응답){
+    console.log(요청.body)
+    요청.body._id = parseInt(요청.body._id);
+    //요청.body에 담겨온  게시물번호를 가진 글을 db에서 찾아서 삭제해주세요.
+    db.collection('post').deleteOne(요청.body ,function(에러, 결과){
+        console.log('삭제완료'); 
+        응답.status(200).send({ message : '성공했다.'});
+    })   
+});    
+app.get('/detail/:id',function(요청,응답){
+    db.collection('post').findOne({_id: parseInt(요청.params.id) },function(에러,결과){
+        console.log(결과); 
+        응답.render('detail.ejs',{ data : 결과}) 
+    })
+    
+
+ 
+})
+app.get('/edit',function(요청,응답){
+    응답.render('edit.ejs');
+    
+})
