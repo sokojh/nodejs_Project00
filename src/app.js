60fe6f03a9bbcd222c7372fd0bb62fce2872d196
kogoome <kogoome@kakao.com>
2021-09-10 20:08:29 +0900

    ㅁㅁ

--- src/app.js
+++ src/app.js
@@ -25,6 +25,13 @@ app.use(methodOverride('_method'))
 mongo.connect()
 const db = mongo.db('weeksom')
 
+// s3 이미지 가져오기
+app.get('/image/:key', (요청, 응답) => {
+  const key = 요청.params.key
+  const readStream = getFileStream(key)
+  readStream.pipe(응답)
+})
+
 // 하나면 홈임
 app.get('/', function (요청, 응답) {
   // 응답.sendfile(__dirname + '/views/index.ejs');
@@ -39,3 +46,28 @@ app.use('/profile', require('../routes/profile'))
 app.use('/search', require('../routes/search'))
 
 module.exports = app
+
+// //app.post('/image',upload.single('프로필'), async (요청,응답) => {
+//     const file = 요청.file
+//     //파일 내용을 확인하는 로그
+//     console.log(file)
+//     //여기서 이미지 키값을 가져옵니다.
+//     const result = await uploadFile(file)
+//     //파일이 업로드 되었을때 확인하는로그
+//     console.log(result)
+//     const description = 요청.body.description
+//     응답.send({imagePath: '/add/${result.key}'})
+//     db.collection('weaksom').findOne({name : '게시물갯수'},function(에러,결과){
+//         console.log(결과.totalPost);
+//         var 총게시물갯수 = 결과.totalPost;
+//         var 저장할거 = { _id : 총게시물갯수 + 1,제목 : 요청.body.title, 날짜 : 요청.body.date , 작성자 : 요청.user._id}
+//         db.collection('post').insertOne(저장할거,function(에러,결과){
+//             console.log('저장완료');
+//             //counter라는 콜렉션에 있는 totalPost라는 항목도 1 증가시켜야함 (수정)
+//             db.collection('counter').updateOne({name : '게시물갯수'},{ $inc : {totalPost:1}},function(에러,결과){
+//                 if(에러) {return console.log(에러)}
+//                 console.log(결과)
+//             });
+//        })
+//     });
+//     });
