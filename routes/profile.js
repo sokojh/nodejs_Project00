3b9d604ee5f99471245180df09ca53d5b4f1a66a
kogoome <kogoome@kakao.com>
2021-09-11 17:53:59 +0900

    a

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,8 @@
 const router = require('express').Router()
 const appfn = require('../src/function_app')
 
-router.use('/new', appfn.로그인했니, (req, res) => {})
-
-router.get('/new', function (요청, 응답) {
-  //응답.sendfile(__dirname + '/views/write.ejs');
-  응답.render('../views/write.ejs')
+router.get('/new', appfn.로그인했니, function (요청, 응답) {
+  응답.render('../views/profile.ejs', { user: req.user })
 })
-
-//router.use(로그인했니); 모든곳에 미들웨어 넣을수있는 매서드
-//router.use('/game',로그인했니);  이거는 특정 /game 접속할때만 미들웨어를 시작하겠다는 뜻
 
 module.exports = router
