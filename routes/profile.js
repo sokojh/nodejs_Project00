3648df34d0e414d9aa96fc5eba8aca181fff2fe1
chanwon moon <kogoome@kakao.com>
2021-09-12 14:58:57 +0900

    초기화

--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const appfn = require('../src/function_app')
-
-router.get('/', function (요청, 응답) {
-  응답.render('../views/profile.ejs')
-})
-
-module.exports = router
