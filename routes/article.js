4fe3bfb3764dca3d5e230786f3a69cdabe4e8ea7
chanwon moon <kogoome@kakao.com>
2021-09-16 08:16:39 +0900

    오타

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,7 @@ const { Article } = require('../api/0.index') // 몽구스 api 임포트
 
 // 게시물 출력 : article : id
 router.get('/', Article.articleRead, (req, res) => {
-  res.render('articleList', { articles: req.Article })
-  console.log(req.Article[0])
+  res.render('articleList', { articles: req.articles })
 })
 
 module.exports = router
