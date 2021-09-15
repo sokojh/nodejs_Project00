22c38a0a5b6e6d5058172d563695a6779715da34
Easy-Hwan <loveetrhffpa@naver.com>
2021-09-15 17:39:04 +0900

    easy

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,7 @@ const { Article } = require('../api/0.index') // 몽구스 api 임포트
 
 // 게시물 출력 : article : id
 router.get('/', Article.articleRead, (req, res) => {
-  console.log(req.Article[3])
-  res.render('list/')
+  res.render('articleList')
 })
 
 module.exports = router
