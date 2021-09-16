bd7fb12d82fefc3b805ce5ae89f9b56b9a4ce600
chanwon moon <kogoome@kakao.com>
2021-09-16 19:37:57 +0900

    라우터 연결, html 수정

--- routes/comment.js
+++ routes/comment.js
@@ -4,11 +4,10 @@ const { Comment } = require('../api/0.index') // 몽구스 api 임포트
 
 // comment 출력
 router.get('/', Comment.commentList, (req, res) => {
-  console.log('유져리스트 라우터 연결')
-  // req.userlist로 모든 유저 정보 가져옴
-  console.log(req.user.weeksomId)
+  console.log('커멘트 라우터 연결')
   res.render('comment.ejs', {
     commentList: req.commentList,
+    userId: req.user.weeksomId,
   })
 })
 
