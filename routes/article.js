bc88218bf64835670daffaa6c4f9833fea565fea
chanwon moon <kogoome@kakao.com>
2021-09-16 19:13:03 +0900

    오류수정2

--- routes/article.js
+++ routes/article.js
@@ -1,13 +1,9 @@
 const router = require('express').Router()
-const { findSourceMap } = require('module')
 const { Article } = require('../api/0.index') // 몽구스 api 임포트
 
 // 게시물 출력 : article : id
 router.get('/', Article.articleRead, (req, res) => {
-  res.render('articleList', { articles: req.Article[3] })
-  console.log(req.Article[3])
-
-  find().populte(user)
+  res.render('articleList', { articles: req.articles })
 })
 
 module.exports = router
