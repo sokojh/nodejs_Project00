a783a0d5d1804f0d5b9b06bdfc15ad4cb3971e05
chanwon moon <kogoome@kakao.com>
2021-09-19 22:00:08 +0900

    유져 스키마 변경

--- mongoose/schema/user.js
+++ mongoose/schema/user.js
@@ -2,17 +2,24 @@ const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const User = new mongoose.Schema({
+  // 로그인정보
   email: { type: String, required: true, unique: true },
   weeksomId: { type: String, required: true, unique: true },
   hashedPassword: { type: String, required: true },
   nickname: { type: String, required: true },
   profileImgKey: { type: String, default: '' },
   userinfo: { type: String, default: '' },
+  // 유져데이터
   following: { type: Array },
   follower: { type: Array },
   article: { type: Array },
-  articleCount: {type:Number, default:0, required:true },
+  likeAticle: { type: Array },
   bookmark: { type: Array },
+  // 카운 데이터
+  followingCount: { type: Number },
+  followerCount: { type: Number },
+  likeCount: { type: Number },
+  bookmarkCount: { type: Number },
   versionKey: false,
 })
 
