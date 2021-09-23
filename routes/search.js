d0bc1e84676f8ac9375802d9c1f0c3a46d7e3af5
soko <sokojiho2@gmail.com>
2021-09-23 13:01:40 +0900

    검색시작

--- routes/search.js
+++ routes/search.js
@@ -1,14 +1,21 @@
 //@ts-check
 const router = require('express').Router()
 const { Article } = require('../api/0.index') // 몽구스 api 임포트
-
+var db
 // 게시물 출력 : article : id
-router.get('/', Article.articlePopRead, (req, res) => {
+//Article.articlePopRead 미들웨어 제거
+router.get('/', (req, res) => {
   console.log('search 라우터 연결')
   // @ts-ignore
   res.render('search', { articles: req.articles })
   // @ts-ignore
   console.log(req.articles)
+  console.log(req.query.value)
+})
+//검색 value값 전용
+router.get('/v', (req, res) => {
+  console.log('벨류값을 보냄')
+  console.log(req.query.value)
 })
 
 module.exports = router
