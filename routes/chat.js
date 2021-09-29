ac05045c2c31332db793b83663555794f8670888
somsomgom <intdeveloper20@gmail.com>
2021-09-29 21:41:34 +0900

    test001

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
+const { User } = require('../api/0.index') // 몽구스 api 임포트
 
 router.get('/', (req, res) => {
   // todo something
   res.render('chat.ejs', { user: req.user })
 })
 
+router.post('/', User.chatUser, (req, res) => {
+  //user.js에 들렀다옴
+  const { weeksomId, profileImgKey } = req.userProfile //form값을 전송하는 코드
+  console.log(weeksomId, profileImgKey)
+  res.send(200, { weeksomId, profileImgKey })
+})
 module.exports = router
